5bedd4572619016a47cffa553d41fe6a57d40e95
Panda-32 <70824051+Panda-32@users.noreply.github.com>
2020-09-15 15:58:52 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,80 +1,79 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 class Main {
-String alphabet="abcdefghijklmnpqrstuvwxyz";
   public static void main(String[] args) {
+    int pennies,nickels,dimes,quaters;
+  final double pennie = 0.01,nickel = 0.05,dime = 0.10 , quater = 0.25;
+  double total;
+  Scanner keyboard = new Scanner(System.in);
+  System.out.println("|MONEY GAMES|");
+  System.out.println("Enter Number Of Pennies");
+  pennies=keyboard.nextInt();
+  System.out.println("Enter Number Of Nickles");
+  nickels=keyboard.nextInt();
+  System.out.println("Enter Number Of Dimes");
+  dimes=keyboard.nextInt();
+  System.out.println("Enter Number Of Quaters");
+  quaters=keyboard.nextInt();
+  total = pennies*pennie + nickel*nickels + dimes*dime + quater*quaters;
+  total = Math.round(total * 100.0) / 100.0;
+  System.out.println("you have " + total);
+  System.out.println("Would you like to buy:");
+  System.out.println("1.icream -3.50");
+  System.out.println("2.panckes - 0.41 ");
+  System.out.println("3.Candy - 0.50 ");
+ int playerchoice = keyboard.nextInt();
+  if(playerchoice == 1)
+  {
+    if(total>3.50  || total ==3.50)
+   {
+     System.out.println("YOu win");
+
+   }
+   else if(total<3.50)
+   {
+     System.out.println("You lose");
+ 
+   }
+   
+  }
+    else if(playerchoice == 2)
+  {
+    if(total>0.41 || total ==0.41)
+   {
+     System.out.println("YOu win");
+
+   }
+   else if(total<0.41)
+   {
+     System.out.println("You lose");
+   }
+ 
+   
+ 
+    else if(playerchoice == 3)
+  {
+    if(total>0.50 || total ==0.50)
+   {
+     System.out.println("YOu win");
+
+   }
+   else if(total<0.50)
+   {
+     System.out.println("You lose");
+   }
+  
+  }  
+
+
+
+
+  
+
+
+
     
-    System.out.println("Welcome to Caser Cipher Encryption tool");
-    System.out.println("Encrypt a word for yoour feirnds to sovle or solve your friends sercret message");
-    System.out.println("All you will need is a key and a message ");
-    Scanner keyInput = new Scanner(System.in);
-    Scanner messageInput = new Scanner(System.in);
-    System.out.println("Pick a key");
-    int key = keyInput.nextInt();
-    System.out.println("Write your message here");
-    String message = messageInput.nextLine();
-    Main encryption = new Main();
-    String newmessage = encryption.encrypt(key , message);
-    System.out.println("your secret message is :");
-    System.out.println(newmessage);
   }
-
-  public int GOPIA(char letter)
-  {
-    return alphabet.indexOf(letter);
-  }
-  public int GEP(int position , int key)
-  {
-    key = key%26;
-    return position+key;
-  }
-  public char GEL(int position)
-  {
-    return alphabet.charAt(position);
-  }
-
-  public boolean isSymbol(String letter)
-  {
-    if (letter.equals("!"))
-    {
-      return true;
-      
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-
-public String encrypt(int key , String message )
-{
-  String encryptedTxt = "";
-  int messagelenght = message.length();
-  for(int i=0; i< messagelenght ; i++)
-  {
-    char letter=message.charAt(i);
-    String l = Character.toString(letter);
-  if( isSymbol(l))
-    {
-      encryptedTxt = encryptedTxt+letter;
-    }
-  if (Character.isWhitespace(letter))
-  {
-    encryptedTxt = encryptedTxt+letter;
-  }
-  else
-  {
-    int alphabetPostion  = GOPIA(letter);
-    int encryptPosition = GEP(alphabetPostion, key);
-    char newLetter = GEL(encryptPosition);
-    encryptedTxt = encryptedTxt+newLetter;
-  }
-
-  }
-return encryptedTxt;
 }
-
-
-
 }
